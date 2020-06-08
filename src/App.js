bdd99d1732c199e9a0dae8f72855dfd5745daad7
Gabriel Nucci <gabriel.nucci@seeaway.com.br>
2020-06-08 17:48:58 -0300

    feat(react): Usando state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 import Pokemon from './Pokemon/Pokemon';
@@ -7,33 +7,42 @@ import Pokemon from './Pokemon/Pokemon';
 Ver como fazer com useEffect e useState
 */
 
-function App() {
-  let pokemons = [];
-  const url = `https://pokeapi.co/api/v2/pokedex/2`;
-  console.log(url);
 
-  const buscarDados = (url) => {
-    fetch(url)
-      .then(
-        (res) => {
-          console.log(res);
-          return res.json();
-        }
-      ).then((pokeLista) => {
-          return pokeLista.pokemon_entries;
-        }
-      )
+class App extends Component {
+  url = `https://pokeapi.co/api/v2/pokedex/2`;
+
+  
+  state = {
+    pokemons: [
+      {name: 'Bulbasaur', id:'001', flavor: 'A strange seed was planted on its back at birth.The plant sprouts and grows with this Pokemon.'},
+      {name: 'Ivysaur', id:'002', flavor: 'When the bulb on its back grows large, it appears to lose the ability to stand on its hind legs'}
+    ],
+    versao: '1.0'
   }
 
-  pokemons = buscarDados(url);
+  switchNameHandler = () => {
+    //console.log('cliquei');
 
-  return (
-    <div className="App">
-      <h1>Olá, estou usando React</h1>
-      <Pokemon poke={{name: 'Bulbasaur', id:'001'}}>A strange seed was planted on its back at birth.The plant sprouts and grows with this Pokemon.</Pokemon>
-      <Pokemon poke={{name: 'Ivysaur', id:'002'}}>When the bulb on its back grows large, it appears to lose the ability to stand on its hind legs.</Pokemon>
-    </div>
-  );
+    this.setState({
+      pokemons: [
+        {name: 'Charmander', id:'004', flavor: 'Obviously prefers hot places. When it rains, steamis said to spout from the tip of its tail.'},
+        {name: 'Charmeleon', id:'005', flavor: 'When it swings its burning tail, it elevates the temperature to unbearably high levels.'}
+      ]
+    });
+
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <h1>Olá, estou usando React</h1>
+        <button onClick={this.switchNameHandler}>Mudar Nome</button>
+        <Pokemon poke={this.state.pokemons[0]}></Pokemon>
+        <Pokemon poke={this.state.pokemons[1]}></Pokemon>
+    <h6>Versão: {this.state.versao}</h6>
+      </div>
+    );
+    }
 }
 
 export default App;
--- src/Pokemon/Pokemon.js
+++ src/Pokemon/Pokemon.js
@@ -3,7 +3,7 @@ import React from 'react';
 const pokemon = (props) => {
     return (<div>
             <p>Id:{props.poke.id} - {props.poke.name}</p>
-            <p>{props.children}</p>
+            <p>{props.poke.flavor}</p>
             </div>);
 }
 
