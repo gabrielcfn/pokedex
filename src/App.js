318f0e650efbb46c96f635ca31a73813ab7381c3
Gabriel Nucci <gabriel.nucci@seeaway.com.br>
2020-06-08 18:37:41 -0300

    feat(react): Implementando hooks Implementando o hook useState e trabalhando com fatias de estado

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import Pokemon from './Pokemon/Pokemon';
@@ -8,41 +8,38 @@ Ver como fazer com useEffect e useState
 */
 
 
-class App extends Component {
-  url = `https://pokeapi.co/api/v2/pokedex/2`;
-
-  
-  state = {
-    pokemons: [
-      {name: 'Bulbasaur', id:'001', flavor: 'A strange seed was planted on its back at birth.The plant sprouts and grows with this Pokemon.'},
-      {name: 'Ivysaur', id:'002', flavor: 'When the bulb on its back grows large, it appears to lose the ability to stand on its hind legs'}
-    ],
-    versao: '1.0'
-  }
-
-  switchNameHandler = () => {
-    //console.log('cliquei');
-
-    this.setState({
+const App = props => {
+    const [pokemonsState, setPokemonsState] = useState({
       pokemons: [
-        {name: 'Charmander', id:'004', flavor: 'Obviously prefers hot places. When it rains, steamis said to spout from the tip of its tail.'},
-        {name: 'Charmeleon', id:'005', flavor: 'When it swings its burning tail, it elevates the temperature to unbearably high levels.'}
+        {name: 'Bulbasaur', id:'001', flavor: 'A strange seed was planted on its back at birth.The plant sprouts and grows with this Pokemon.'},
+        {name: 'Ivysaur', id:'002', flavor: 'When the bulb on its back grows large, it appears to lose the ability to stand on its hind legs'}
       ]
     });
 
-  }
-  
-  render() {
+    const [infoState, setInfoState] = useState({
+        versao: '1.0',
+        url: `https://pokeapi.co/api/v2/pokedex/2`
+    })
+
+    const switchNameHandler = () => {
+      setPokemonsState({
+        pokemons: [
+          {name: 'Charmander', id:'004', flavor: 'Obviously prefers hot places. When it rains, steamis said to spout from the tip of its tail.'},
+          {name: 'Charmeleon', id:'005', flavor: 'When it swings its burning tail, it elevates the temperature to unbearably high levels.'}
+        ]
+      });
+    }
+
     return (
       <div className="App">
         <h1>Olá, estou usando React</h1>
-        <button onClick={this.switchNameHandler}>Mudar Nome</button>
-        <Pokemon poke={this.state.pokemons[0]}></Pokemon>
-        <Pokemon poke={this.state.pokemons[1]}></Pokemon>
-    <h6>Versão: {this.state.versao}</h6>
+        <button onClick={switchNameHandler}>Mudar Nome</button>
+        <Pokemon poke={pokemonsState.pokemons[0]}></Pokemon>
+        <Pokemon poke={pokemonsState.pokemons[1]}></Pokemon>
+      <h6>Versão: {infoState.versao}</h6>
       </div>
     );
-    }
+    
 }
 
 export default App;
